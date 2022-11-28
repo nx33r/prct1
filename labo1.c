588356ec19821d462e6aad6cafc13bb0be0afb3e
nx33r <nazarii.lelyk.ir.2022@lpnu.ua>
2022-11-28 13:42:55 +0100

    adding my first coment

--- labo1.c
+++ labo1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 void main(){
-    double x = 0.013;
+    double x = 0.013; //coment//
     double z = 1.245;
     printf("%f", (sin(pow(x, cos(z))) + pow(tan(z), sin(x))));
 }
